b84a8058974b9bcda38f7c8dc6d029946e853d52
Darknight47 <farhadgunners@gmail.com>
2026-07-10 22:19:12 +0200

    greedy, implementation

---
+++ Hungry.py
@@ -0,0 +1,38 @@
+"""
+
+------------------------------------ Link for the challenge: https://codeforces.com/problemset/problem/903/A ------------------------------------
+
+Ivan's classes at the university have just finished, and now he wants to go to the local CFK cafe and eat some fried chicken.
+
+CFK sells chicken chunks in small and large portions. A small portion contains 3 chunks; a large one — 7 chunks. Ivan wants to eat exactly x chunks. 
+Now he wonders whether he can buy exactly this amount of chicken.
+
+Formally, Ivan wants to know if he can choose two non-negative integers a and b in such a way that a small portions and b large ones contain exactly x chunks.
+
+Help Ivan to answer this question for several values of x!
+
+Input
+The first line contains one integer n (1 ≤ n ≤ 100) — the number of testcases.
+
+The i-th of the following n lines contains one integer xi (1 ≤ xi ≤ 100) — the number of chicken chunks Ivan wants to eat.
+
+Output
+Print n lines, in i-th line output YES if Ivan can buy exactly xi chunks. Otherwise, print NO.
+
+Input:
+2
+6
+5
+
+Output:
+YES
+NO
+"""
+cases = int(input())
+for _ in range(cases):
+    x = int(input())
+    if(x == 1 or x == 2 or x == 4 or x == 5
+       or x == 8 or x == 11):
+        print("NO")
+    else:
+        print("YES")
