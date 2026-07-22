04f274e904441a7f877d661865aae0c6f46f69cc
Darknight47 <farhadgunners@gmail.com>
2026-07-22 21:58:24 +0200

    implementation

---
+++ Free.py
@@ -0,0 +1,42 @@
+"""
+
+-------------------------------------------- Link for the challenge: https://codeforces.com/problemset/problem/237/A ---------------------------------------
+
+Valera runs a 24/7 fast food cafe. He magically learned that next day n people will visit his cafe. 
+For each person we know the arrival time: the i-th person comes exactly at hi hours mi minutes. 
+The cafe spends less than a minute to serve each client, but if a client comes in and sees that there is no free cash, than he doesn't want to wait and leaves the cafe immediately.
+
+Valera is very greedy, so he wants to serve all n customers next day (and get more profit). 
+However, for that he needs to ensure that at each moment of time the number of working cashes is no less than the number of clients in the cafe.
+
+Help Valera count the minimum number of cashes to work at his cafe next day, so that they can serve all visitors.
+
+Input
+The first line contains a single integer n (1 ≤ n ≤ 10^5), that is the number of cafe visitors.
+
+Each of the following n lines has two space-separated integers hi and mi (0 ≤ hi ≤ 23; 0 ≤ mi ≤ 59), representing the time when the i-th person comes into the cafe.
+
+Note that the time is given in the chronological order. All time is given within one 24-hour period.
+
+Output
+Print a single integer — the minimum number of cashes, needed to serve all clients next day.
+
+Input:
+4
+8 0
+8 10
+8 10
+8 45
+
+Output:
+2
+"""
+n = int(input())
+diction = {}
+for _ in range(n):
+    tmp = input()
+    if(tmp not in diction):
+        diction[tmp] = 1
+    else:
+        diction[tmp] += 1
+print(max(diction.values()))
