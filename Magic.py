c6115e71d2a9e81c2b7b1198c5ec051e159ffd88
Darknight47 <farhadgunners@gmail.com>
2026-08-25 12:04:46 +0200

    math

---
+++ Magic.py
@@ -0,0 +1,56 @@
+"""
+
+--------------------------------------------- Link for the challenge: https://codeforces.com/problemset/problem/1257/B ------------------------------------------------
+
+Recently Petya walked in the forest and found a magic stick.
+
+Since Petya really likes numbers, the first thing he learned was spells for changing numbers. So far, he knows only two spells that can be applied to a positive integer:
+
+If the chosen number a is even, then the spell will turn it into 3*a/2;
+If the chosen number a is greater than one, then the spell will turn it into a−1.
+Note that if the number is even and greater than one, then Petya can choose which spell to apply.
+
+Petya now has only one number x. 
+He wants to know if his favorite number y can be obtained from x using the spells he knows. 
+The spells can be used any number of times in any order. It is not required to use spells, Petya can leave x as it is.
+
+Input
+The first line contains single integer T (1 ≤ T ≤ 10^4) — the number of test cases. Each test case consists of two lines.
+
+The first line of each test case contains two integers x and y (1 ≤ x, y ≤ 10^9) — the current number and the number that Petya wants to get.
+
+Output
+For the i-th test case print the answer on it — YES if Petya can get the number y from the number x using known spells, and NO otherwise.
+
+You may print every letter in any case you want (so, for example, the strings yEs, yes, Yes and YES will all be recognized as positive answer).
+
+Input:
+7
+2 3
+1 1
+3 6
+6 8
+1 2
+4 1
+31235 6578234
+
+Output:
+YES
+YES
+NO
+YES
+NO
+YES
+YES
+"""
+cases = int(input())
+for _ in range(cases):
+    x, y = map(int, input().split())
+    ok = False
+    if(x >= y):
+        ok = True
+    elif(x == 2 and y == 3):
+        ok = True
+    elif(x >= 4):
+        ok = True
+    print("YES" if ok else "NO")
