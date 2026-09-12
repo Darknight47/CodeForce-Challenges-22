d8417cdc1929d4942d511b79d7d74f8ac57bf35f
Darknight47 <farhadgunners@gmail.com>
2026-09-12 14:24:14 +0200

    brute force, greedy, two pointers

---
+++ Moo.py
@@ -0,0 +1,57 @@
+"""
+
+------------------------------------------------ Link for the challenge: https://codeforces.com/problemset/problem/2259/A -----------------------------------------
+
+Farmer John is trying to increase literacy rates in the United Cows of Farmer John (UCFJ). 
+The UCFJ consists of n fields and nk farms (where n is a multiple of k), with each farm consisting of k consecutive fields. 
+In other words, the i-th field is in the ⌈ik⌉-th farm: Fields 1,2,…,k are in the first farm, fields k+1,k+2,…,2k are in the second farm, etc.
+
+Farmer John wants to build schools such that each farm has at least one school. 
+However, some fields are owned by Farmer Nhoj, who will charge Farmer John extra to build a school there. 
+Farmer John wants to know the minimum number of times that he would have to build a school on Farmer Nhoj's land in order to ensure that each farm has at least one school.
+
+Input
+The first line of each input contains an integer t (1≤t≤10^4) — the number of test cases.
+
+The first line of each test case contains two integers n and k (1≤k≤n≤20, n is a multiple of k) — the number of fields and size of each farm.
+
+The second line of each test case contains a binary string s of length n— the fields owned by Farmer Nhoj. If si=1, the i-th field is owned by Farmer Nhoj. If si=0, the i-th field is not owned by Farmer Nhoj.
+
+Output
+For each test case, output a single integer — the minimum number of times that Farmer John must build a school on Farmer Nhoj's land.
+
+Input:
+6
+8 2
+10011100
+5 1
+11111
+8 4
+01111110
+5 1
+00101
+4 4
+1101
+4 4
+1111
+
+Output:
+1
+5
+0
+2
+0
+1
+"""
+cases = int(input())
+for _ in range(cases):
+    n, k = map(int, input().split())
+    s = input()
+    temp = n // k
+    substrings = [s[i: i + k] for i in range(0, temp * k, k)]
+
+    ans = 0
+    for substring in substrings:
+        if(substring.count('1') == k):
+            ans += 1
+    print(ans)
